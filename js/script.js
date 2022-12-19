ce2965d434c01377c5860f4ddf65b8cfbc9126eb
Marco 9726 <marcosarra9@gmail.com>
2022-12-19 18:12:33 +0100

    (bonus) aggiunta immagine

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ const {
 
 createApp({
     data(){
-        return{
-            title: 'Titolo Esercizio'
+        return {
+            title: 'TITOLO ESERCIZIO',
+            url: 'https://media-assets.wired.it/photos/630dd42a934a411e4c04a538/16:9/w_2560%2Cc_limit/House-of-the-Dragon-CGI-Culture.jpg'
+
         }
     }
-}).mount('#app') 
+}).mount('#app')
